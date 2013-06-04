6179daa9c5f447b89891131aba96d49a9b53583c
Eric Xu <e.xulander@gmail.com>
2013-06-03 19:25:18 -0700

    set up initial product model and views; finish Task A

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PartIIDepot::Application.config.secret_token = '2989577c2b4be8776d9903af81a41c9a2a91405eb6a7a86b60045720563fca9796e1004ef4d0113c366131ce0668d7d2754ab32875dcd47f869fbc1b63e37a5a'
