fa71f8cac82e0d94766ab6ff5157ea40ad21db3e
Eric Xu <e.xulander@gmail.com>
2013-06-07 16:06:31 -0700

    write unit tests for product model; finish Task B

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
 
-  validates :title, :description, :image_url, presence: true
+  validates :title, :description, :image_url, presence: { message: "^Did you forget the title?" }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
-  validates :title, uniqueness: true
+  validates :title, uniqueness: { message: "^%{value} has already been taken. Sorry." }
+  validates :title, length: { minimum: 10, message: "is too short" }
   validates :image_url, format: {
   	with: %r{\.(gif|jpg|png)$}i,
   	message: 'must be a URL for GIF, JPG, or PNG image.'
