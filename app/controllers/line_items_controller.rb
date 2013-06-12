a85d1838c7fa255051d3fa8d1292fcb883ffc18a
Eric Xu <e.xulander@gmail.com>
2013-06-11 19:18:43 -0700

    add a button to empty cart; tidy up cart show page; finish TaskE

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -50,7 +50,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
+        format.html { redirect_to @line_item.cart, notice: 'Successfullly added product.'}
         format.json { render json: @line_item, status: :created, location: @line_item }
       else
         format.html { render action: "new" }
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,4 +11,8 @@ class Cart < ActiveRecord::Base
   	end
   	current_item
   end
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
