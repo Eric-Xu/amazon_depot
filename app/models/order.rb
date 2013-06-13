346e3e94d375d72ea1e347d3863221a33e0fa5f4
Eric Xu <e.xulander@gmail.com>
2013-06-13 16:32:00 -0700

    update controller, validations, views, and css for new order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ActiveRecord::Base
   attr_accessible :address, :email, :name, :pay_type
+
+  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+
+  validates :name, :address, :email, :pay_type, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
