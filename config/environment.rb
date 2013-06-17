a81508b392fea3aa1c159dffe39bef3e1bb2392f
Eric Xu <e.xulander@gmail.com>
2013-06-17 15:45:39 -0700

    write integration test for user buying a product; finish Task H

--- config/environment.rb
+++ config/environment.rb
@@ -3,17 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 PartIIDepot::Application.initialize!
-
-# PartIIDepot::Application.configure do
-# 	config.action_mailer.delivery_method = :smtp
-
-# 	config.action_mailer.smtp_settings = {
-# 		:address => "smtp.gmail.com",
-# 		:port => 587,
-# 		:domain	=> "domain.of.sender.net",
-# 		:authentication => "plain",
-# 		:user_name => "EricTurnsThirty",
-# 		:password => "30feelsgood",
-# 		:enable_starttls_auto => true
-# 	}
-# end
