a1c2ba0aee13fbf68b0e9deb3f96c6de5db9f403
Eric Xu <e.xulander@gmail.com>
2013-06-18 15:32:39 -0700

    create sessions controller/views and admin controller/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 PartIIDepot::Application.routes.draw do
+  get "admin" => "admin#index"
+
+  controller :sessions do
+    get "login" => :new
+    post "login" => :create
+    delete "logout" => :destroy
+  end
+
   resources :users
-
-
   resources :orders
-
-
   resources :line_items
-
-
   resources :carts
-
 
   get "store/index"
 
